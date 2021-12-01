fcd81823d0d8994186fb5a8f8c554cacbb8723d9
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2021-12-01 13:18:06 +0900

    Upload code before clear cache

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Tailwind from "./Tailwind.svelte";
+</script>
+
+<Tailwind />
+<main>
+  <h1 class="text-blue-400">
+    yarn berry / scss / typescript / tailwind CSS template
+  </h1>
+</main>
+
+<style lang="scss">
+  main {
+    h1 {
+      font-size: 2rem;
+      text-align: center;
+    }
+  }
+</style>
